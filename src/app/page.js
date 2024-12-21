490ef343f38acfce7e7dcfafac1d5f19de88be68
Vishtheendodoc <dr.vishu.sp@gmail.com>
2024-12-21 19:18:26 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
         <div>
-            <h1>Welcome to My Management App!</h1>
-            <p>This is your custom homepage.</p>
+            <h1>Welcome to Dr Vishwanath Diabetes Management App!</h1>
+            <p>One site to know about insulin titration.</p>
         </div>
     );
 }
